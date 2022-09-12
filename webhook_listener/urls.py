c452c754d0268879e0a5d60ecd6fa60a5977d19c
HitmanCodes <y.vardhan1234@outlook.com>
2022-09-12 23:27:49 +0530

    static serve

--- webhook_listener/urls.py
+++ webhook_listener/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-# from django.conf.urls import url
-# from django.conf import settings
-# from django.views.static import serve
+from django.urls.conf import re_path
+from django.conf import settings
+from django.views.static import serve
 
 
 urlpatterns = [
     path('',include('frontend.urls')),
     path('listen/',include('api.urls')),
     path('admin/', admin.site.urls),
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
